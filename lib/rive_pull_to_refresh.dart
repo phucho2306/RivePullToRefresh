75006c4abfee2b614cfd1ab2ec6f7c62b3a872c1
PhucHo <hophuc236@gmail.com>
2024-03-19 22:35:30 +0700

    dart format .

--- lib/rive_pull_to_refresh.dart
+++ lib/rive_pull_to_refresh.dart
@@ -137,7 +137,8 @@ class RivePullToRefresh extends StatefulWidget {
   State<RivePullToRefresh> createState() => _RivePullToRefreshState();
 }
 
-class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProviderStateMixin<RivePullToRefresh> {
+class _RivePullToRefreshState extends State<RivePullToRefresh>
+    with TickerProviderStateMixin<RivePullToRefresh> {
   late AnimationController _positionController;
   late Animation<double> _positionFactor;
   late Animatable<double> _kDragSizeFactorLimitTween;
@@ -146,7 +147,8 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
   @override
   void initState() {
     super.initState();
-    _kDragSizeFactorLimitTween = Tween<double>(begin: 0.0, end: widget.dragSizeFactorLimitMax);
+    _kDragSizeFactorLimitTween =
+        Tween<double>(begin: 0.0, end: widget.dragSizeFactorLimitMax);
     if (widget.percentActiveBump <= 0.0 || widget.percentActiveBump > 1.0) {
       log("[percentActiveBump] not correct. this value range from 0 to 100");
       throw Error();
@@ -155,7 +157,9 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
     _positionController = AnimationController(vsync: this);
     _positionFactor = _positionController.drive(_kDragSizeFactorLimitTween);
     _controller = RivePullToRefreshController(
-        onRefreshI: widget.onRefresh, controller: widget.controller, positionController: _positionController);
+        onRefreshI: widget.onRefresh,
+        controller: widget.controller,
+        positionController: _positionController);
     widget.onInit(_controller);
   }
 
@@ -163,18 +167,22 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
     if (completer != null) {
       return false;
     }
-    if (notification is ScrollStartNotification && notification.metrics.pixels == 0) {
+    if (notification is ScrollStartNotification &&
+        notification.metrics.pixels == 0) {
       _shouldStart = true;
     }
-    if (notification.metrics.pixels > 0 && _controller._rivePullToRefreshState == null) {
+    if (notification.metrics.pixels > 0 &&
+        _controller._rivePullToRefreshState == null) {
       _shouldStart = false;
     }
-    if ((notification is ScrollUpdateNotification || notification is OverscrollNotification) &&
+    if ((notification is ScrollUpdateNotification ||
+            notification is OverscrollNotification) &&
         _controller._rivePullToRefreshState != null &&
         _shouldStart == true) {
       // calculator position here
       if (notification is ScrollUpdateNotification) {
-        _controller._dragOffset = _controller._dragOffset + notification.scrollDelta!;
+        _controller._dragOffset =
+            _controller._dragOffset + notification.scrollDelta!;
 
         //When the user pulls up a little, it is still a accepted
         if (_positionController.value <= 0.95) {
@@ -182,15 +190,18 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
         }
       }
       if (notification is OverscrollNotification) {
-        _controller._dragOffset = _controller._dragOffset + notification.overscroll;
+        _controller._dragOffset =
+            _controller._dragOffset + notification.overscroll;
         if (_positionController.value >= (widget.percentActiveBump)) {
           _controller._rivePullToRefreshState = RivePullToRefreshState.accept;
         }
       }
-      double newValue =
-          (_controller._dragOffset) / (notification.metrics.viewportDimension * widget.kDragContainerExtentPercentage);
+      double newValue = (_controller._dragOffset) /
+          (notification.metrics.viewportDimension *
+              widget.kDragContainerExtentPercentage);
       if (_controller._oldValue != null) {
-        var value = _positionController.value + (_controller._oldValue! - newValue);
+        var value =
+            _positionController.value + (_controller._oldValue! - newValue);
 
         _positionController.value = clampDouble(value, 0.0, 1.0);
 
@@ -213,7 +224,8 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
     if (_controller._rivePullToRefreshState == RivePullToRefreshState.accept) {
       widget.onMoveToPositionBumpStart?.call();
       await _positionController.animateTo(widget.sizeFactorLimitMin,
-          duration: widget.timeResize, curve: widget.curveMoveToPositionBumpStart!);
+          duration: widget.timeResize,
+          curve: widget.curveMoveToPositionBumpStart!);
       await widget.bump?.call();
     } else {
       await _controller._close(jumpTo: jumpTo);
@@ -238,7 +250,8 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
           } else {
             if (_controller._rivePullToRefreshState == null) {
               // action first pull Overscroll to active refresh
-              _controller._rivePullToRefreshState = RivePullToRefreshState.cancel;
+              _controller._rivePullToRefreshState =
+                  RivePullToRefreshState.cancel;
               return true;
             }
           }
@@ -267,8 +280,11 @@ class _RivePullToRefreshState extends State<RivePullToRefresh> with TickerProvid
               : Column(
                   children: [
                     SizeTransition(
-                      axisAlignment: _controller._rivePullToRefreshState == null ? 1.0 : -1.0,
-                      sizeFactor: _positionFactor, // this is what brings it down
+                      axisAlignment: _controller._rivePullToRefreshState == null
+                          ? 1.0
+                          : -1.0,
+                      sizeFactor:
+                          _positionFactor, // this is what brings it down
                       child: AnimatedBuilder(
                         animation: _positionController,
                         builder: (BuildContext context, Widget? _) {
